da7ee089e2a6b418d5fe53838926ef9897b3bee4
Yamadashoei <k023g0053@g.neec.ac.jp>
2024-10-28 15:14:21 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,68 +4,6 @@
 
 const char kWindowTitle[] = "LE2C_26_ヤマダショウエイ_MT4_01_01";
 
-struct Vector3 {
-	float x, y, z;
-};
-
-struct Matrix4x4 {
-	float m[4][4];
-};
-
-float Dot(const Vector3& v1, const Vector3& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
-float Length(const Vector3& v) { return std::sqrt(Dot(v, v)); }
-
-
-Vector3 Normalize(const Vector3& v) {
-	float length = Length(v);
-	assert(length != 0.0f);
-	return { v.x / length, v.y / length, v.z / length };
-}
-
-Matrix4x4 MakeRotateAxisAngle(const Vector3& axis, float angle) {
-
-
-	//資料p20を参考に中身を埋める。nはaxisのこと
-	float cos = std::cos(angle);
-	float sin = std::sin(angle);
-
-	Matrix4x4 rotateMatrix = {};
-	rotateMatrix.m[0][0] =
-		rotateMatrix.m[0][1] =
-		rotateMatrix.m[0][2] =
-		rotateMatrix.m[0][3] =
-
-		rotateMatrix.m[1][0] =
-		rotateMatrix.m[1][1] =
-		rotateMatrix.m[1][2] =
-		rotateMatrix.m[1][3] =
-
-		rotateMatrix.m[2][0] =
-		rotateMatrix.m[2][1] =
-		rotateMatrix.m[2][2] =
-		rotateMatrix.m[2][3] =
-
-		rotateMatrix.m[3][0] =
-		rotateMatrix.m[3][1] =
-		rotateMatrix.m[3][2] =
-		rotateMatrix.m[3][3] =
-
-		return rotateMatrix;
-}
-
-static const int kRowHeight = 20;
-static const int kColumnWidth = 60;
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
-	Novice::ScreenPrintf(x, y, "%s", label);
-	for (int row = 0; row < 4; ++row) {
-		for (int column = 0; column < 4; ++column) {
-			Novice::ScreenPrintf(
-				x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.03f",
-				matrix.m[row][column]);
-		}
-	}
-}
-
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -92,9 +30,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Vector3 axis = Normalize(Vector3{ 1.0f, 1.0f, 1.0f });
-		float angle = 0.44f;
-		Matrix4x4 rotateMatrix = MakeRotateAxisAngle(axis, angle);
 
 		///
 		/// ↑更新処理ここまで
@@ -104,8 +39,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(0, 0, rotateMatrix, "rotateMatrix");
-
+		
 		///
 		/// ↑描画処理ここまで
 		///
