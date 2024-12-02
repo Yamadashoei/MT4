f6f9a41c171412518025b7c64ea8ea2ef5bd9818
Yamadashoei <k023g0053@g.neec.ac.jp>
2024-12-02 10:08:28 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,21 +1,56 @@
 #include <Novice.h>
-#include <cassert>
-#include <cmath>
 
-const char kWindowTitle[] = "LE2C_26_ヤマダショウエイ_MT4_01_01";
+const char kWindowTitle[] = "LE2C_26_ヤマダ_ショウエイ";
 
+static const int kRowHeight = 20;
+static const int kColumnWidth = 70;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name) {
+	Novice::ScreenPrintf(x, y, "%s", name);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + 20 + row * kRowHeight, "%.03f", matrix.m[row][column]);
+		}
+	}
+}
+
+void QuaternionScreenPrintf(int x, int y, const Quaternion& quaternion, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", quaternion.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", quaternion.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", quaternion.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%.02f", quaternion.w);
+	Novice::ScreenPrintf(x + kColumnWidth * 4, y, "%s", label);
+}
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	const int kWindowWidth = 1280;
-	const int kWindowHeight = 720;
-	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	Quaternion rotation0 = MakeRotateAxisAngleQuaternion({ 0.71f,0.71f,0.0f }, 0.3f);
+	Quaternion rotation1 = MakeRotateAxisAngleQuaternion({ 0.71f,0.0f,0.71f }, 3.141592f);
+
+	Quaternion interpolate0 = Slerp(rotation0, rotation1, 0.0f);
+	Quaternion interpolate1 = Slerp(rotation0, rotation1, 0.3f);
+	Quaternion interpolate2 = Slerp(rotation0, rotation1, 0.5f);
+	Quaternion interpolate3 = Slerp(rotation0, rotation1, 0.7f);
+	Quaternion interpolate4 = Slerp(rotation0, rotation1, 1.0f);
+
+
+
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -30,7 +65,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -39,7 +73,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		
+		QuaternionScreenPrintf(0, kRowHeight * 0, interpolate0, ": interpolate0,Slerp(q0,q1,0.0f)");
+		QuaternionScreenPrintf(0, kRowHeight * 1, interpolate1, ": interpolate0,Slerp(q0,q1,0.3f)");
+		QuaternionScreenPrintf(0, kRowHeight * 2, interpolate2, ": interpolate0,Slerp(q0,q1,0.5f)");
+		QuaternionScreenPrintf(0, kRowHeight * 3, interpolate3, ": interpolate0,Slerp(q0,q1,0.7f)");
+		QuaternionScreenPrintf(0, kRowHeight * 4, interpolate4, ": interpolate0,Slerp(q0,q1,1.0f)");
+
+
 		///
 		/// ↑描画処理ここまで
 		///
